afd59552eae66232dc621b8fc7821fcdf36e6ab9
Sukarna Dey <47328474+S-Brishty@users.noreply.github.com>
2026-05-14 02:04:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+hereconst apiKey = "YOUR_API_KEY_HERE";
 
+function getWeather() {
+  const city = document.getElementById("city").value;
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("cityName").innerText = data.name;
+      document.getElementById("temp").innerText = "Temp: " + data.main.temp + "°C";
+      document.getElementById("desc").innerText = "Weather: " + data.weather[0].description;
+    })
+    .catch(() => {
+      alert("City not found!");
+    });
+        }
